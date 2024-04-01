fb728e56623359a2c3fb55be8649870a05f75287
Avi Cholakova <Cholakova237@gmail.com>
2024-04-01 14:01:22 +0300

    New

---
+++ S03-Modul3/P01-Classes/Family.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_Classes
+{
+    internal class Family
+    {
+        public List<Person> Persons = new List<Person>();
+    }
+}
